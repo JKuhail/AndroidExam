335e6e54d1dbac22afb4ebfb81a7b305ba6e3bc7
Jehad <j.kuhail@unitone.co>
2021-05-27 09:44:03 +0300

    Update MainActivity.kt

--- app/src/main/java/com/jkuhail/androidexam/activitiy/MainActivity.kt
+++ app/src/main/java/com/jkuhail/androidexam/activitiy/MainActivity.kt
@@ -3,9 +3,15 @@ package com.jkuhail.androidexam.activitiy
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.transition.TransitionManager
 import com.jkuhail.androidexam.R
 
 import com.jkuhail.androidexam.databinding.ActivityMainBinding
@@ -14,153 +20,54 @@ import com.jkuhail.androidexam.fragment.FeedFragment
 import com.jkuhail.androidexam.fragment.PreziFragment
 import com.jkuhail.androidexam.fragment.ProfileFragment
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity(){
 
     private lateinit var binding: ActivityMainBinding
-
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        selectFeed()
-
-        binding.layoutFeed.setOnClickListener(this)
-        binding.layoutMyPrezi.setOnClickListener(this)
-        binding.layoutGood.setOnClickListener(this)
-        binding.layoutProfile.setOnClickListener(this)
+        setUpNavigation()
 
     }
 
 
-    private fun refreshNavigationBar() {
-        binding.feedTv.setTextColor(ContextCompat.getColor(applicationContext, R.color.grey_color))
-        binding.feedImg.setColorFilter(
-            ContextCompat.getColor(
-                applicationContext,
-                R.color.grey_color
-            )
+    private fun setUpNavigation() {
+        navController = findNavController(R.id.nav_host_fragment_bottom)
+        NavigationUI.setupWithNavController(
+            binding.bottomNavigationView,
+            navController
         )
-        binding.line1.visibility = View.INVISIBLE
-        binding.myPreziTv.setTextColor(
-            ContextCompat.getColor(
-                applicationContext,
-                R.color.grey_color
-            )
-        )
-        binding.myPreziImg.setColorFilter(
-            ContextCompat.getColor(
-                applicationContext,
-                R.color.grey_color
-            )
-        )
-        binding.line2.visibility = View.INVISIBLE
-        binding.goodTv.setTextColor(ContextCompat.getColor(applicationContext, R.color.grey_color))
-        binding.goodImg.setColorFilter(
-            ContextCompat.getColor(
-                applicationContext,
-                R.color.grey_color
-            )
-        )
-        binding.line3.visibility = View.INVISIBLE
-        binding.profileTv.setTextColor(
-            ContextCompat.getColor(
-                applicationContext,
-                R.color.grey_color
-            )
-        )
-        binding.profileImg.setColorFilter(
-            ContextCompat.getColor(
-                applicationContext,
-                R.color.grey_color
-            )
-        )
-        binding.line4.visibility = View.INVISIBLE
-    }
-
-    private fun replaceFragment(fragment: Fragment?) {
-        val transaction: FragmentTransaction =
-            supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.container, fragment!!).commit()
-    }
-
-    private fun selectFeed() {
-        refreshNavigationBar()
-        binding.feedTv.setTextColor(ContextCompat.getColor(applicationContext, R.color.colorAccent))
-        binding.feedImg.setColorFilter(
-            ContextCompat.getColor(
-                applicationContext,
-                R.color.colorAccent
-            )
-        )
-        binding.line1.visibility = View.VISIBLE
-        replaceFragment(FeedFragment())
-    }
-
-    private fun selectPrezi() {
-        refreshNavigationBar()
-        binding.myPreziTv.setTextColor(
-            ContextCompat.getColor(
-                applicationContext,
-                R.color.colorAccent
-            )
-        )
-        binding.myPreziImg.setColorFilter(
-            ContextCompat.getColor(
-                applicationContext,
-                R.color.colorAccent
-            )
-        )
-        binding.line2.visibility = View.VISIBLE
-        replaceFragment(PreziFragment())
-    }
-
-    private fun selectGoodToKnow() {
-        refreshNavigationBar()
-        binding.goodTv.setTextColor(ContextCompat.getColor(applicationContext, R.color.colorAccent))
-        binding.goodImg.setColorFilter(
-            ContextCompat.getColor(
-                applicationContext,
-                R.color.colorAccent
-            )
-        )
-        binding.line3.visibility = View.VISIBLE
-        replaceFragment(DoingGoodFragment())
-    }
-
-    private fun selectProfile() {
-        refreshNavigationBar()
-        binding.profileTv.setTextColor(
-            ContextCompat.getColor(
-                applicationContext,
-                R.color.colorAccent
-            )
-        )
-        binding.profileImg.setColorFilter(
-            ContextCompat.getColor(
-                applicationContext,
-                R.color.colorAccent
-            )
-        )
-        binding.line4.visibility = View.VISIBLE
-        replaceFragment(ProfileFragment())
-    }
-
-    override fun onClick(v: View?) {
-        when (v!!.id) {
-            R.id.layoutFeed -> {
-                selectFeed()
-            }
-            R.id.layoutMyPrezi -> {
-                selectPrezi()
-            }
-            R.id.layoutGood -> {
-                selectGoodToKnow()
-            }
-            R.id.layoutProfile -> {
-                selectProfile()
-            }
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            underlineSelectedItem(binding.constraintLayout, item.itemId)
+            true
         }
     }
+
+    private fun underlineSelectedItem(view: View, itemId: Int) {
+        val constraintLayout: ConstraintLayout = view as ConstraintLayout
+        TransitionManager.beginDelayedTransition(constraintLayout)
+        val constraintSet = ConstraintSet()
+        constraintSet.clone(constraintLayout)
+        constraintSet.setHorizontalBias(
+            R.id.underline,
+            getItemPosition(itemId) * 0.33f
+        )
+        constraintSet.applyTo(constraintLayout)
+    }
+
+    private fun getItemPosition(itemId: Int): Int {
+        return when (itemId) {
+            R.id.feed -> 0
+            R.id.prezi -> 1
+            R.id.doing_good -> 2
+            R.id.profile -> 3
+            else -> 0
+        }
+    }
+
+
 }
